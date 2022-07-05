41bf22e9ffb28a14b0abbb9412169e15db180ba8
Khalid Talakshi <khalid.talakshi@outlook.com>
2022-07-04 23:02:08 -0400

    Added basic user resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_04_214958) do
+ActiveRecord::Schema.define(version: 2022_07_05_024229) do
 
   create_table "players", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2022_07_04_214958) do
     t.string "name"
     t.string "division"
     t.integer "points", default: 0
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
