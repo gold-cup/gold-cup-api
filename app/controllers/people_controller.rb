7e9eeda19f07d934a59f368f8f5f40c13f960b47
Khalid Talakshi <khalid.talakshi@outlook.com>
2022-07-25 21:53:55 -0400

    bug fix

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,7 @@
 class PeopleController < ApplicationController
   def create
-    user_id = decode_token(request)["user_id"]
+    token_from_request = request.headers["Authorization"]
+    user_id = decode_token(token_from_request)["user_id"]
     person = Person.new(person_params.merge(user_id: user_id, status: "pending"))
     if (person.save)
       render json: person, status: 201
@@ -37,7 +38,7 @@ class PeopleController < ApplicationController
   end
 
   def files
-    check_if_user_owns_person_cookie
+    check_if_user_owns_person_params
     type = params[:type]
     person = Person.find(params[:id])
     if type == 'waiver'
@@ -78,6 +79,7 @@ class PeopleController < ApplicationController
   def decode_token(token)
     begin
       encoded_token = token
+      puts encoded_token
       if (encoded_token)
         token = encoded_token.split(' ').last
         decoded_token = JWT.decode(token, ENV['APP_SECRET'], true, algorithm: 'HS256')
@@ -101,12 +103,13 @@ class PeopleController < ApplicationController
     end
   end
 
-  def check_if_user_owns_person_cookie
-    token_from_cookie = cookies[:token]
-    user_id = decode_token(token_from_request)["user_id"]
+  def check_if_user_owns_person_params
+    token_from_params = params[:token]
+    user_id = decode_token(token_from_params)["user_id"]
     person = Person.find(params[:id])
     if (person.user_id != user_id)
       render json: {error: "You don't have permission to do that"}, status: 401
       return
     end
+  end
 end
