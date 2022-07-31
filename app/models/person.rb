29b6af648c16d6569c1d6c06809ad833c3b23539
Khalid Talakshi <khalid.talakshi@outlook.com>
2022-07-30 22:40:37 -0400

    created age method to get age

--- app/models/person.rb
+++ app/models/person.rb
@@ -26,4 +26,14 @@ class Person < ApplicationRecord
   validates :status, inclusion: {
     in: STATUSES
   }
+
+  validates :players, length: { minimum: 0, maximum: 2 }
+
+  scope :filter_by_status, -> (status) { where status: status }
+
+  def age
+    dob = birthday
+    now = Date.parse('05-08-2022')
+    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
+  end
 end
